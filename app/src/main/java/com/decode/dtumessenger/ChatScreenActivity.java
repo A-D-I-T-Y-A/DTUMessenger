f5f79c910d343c3ac093875d216f2e178be549e3
aanyajindal <aanyajindal05@gmail.com>
2017-02-25 13:08:08 +0530

    formatted chat screen

--- app/src/main/java/com/decode/dtumessenger/ChatScreenActivity.java
+++ app/src/main/java/com/decode/dtumessenger/ChatScreenActivity.java
@@ -1,5 +1,6 @@
 package com.decode.dtumessenger;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class ChatScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_screen);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setCustomView(R.layout.custom_action_bar);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setTitle("Name");
     }
 }
