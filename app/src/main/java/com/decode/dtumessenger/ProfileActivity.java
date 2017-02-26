f23f2994921cf8a7fd76f5a8fd622a5316331241
A-D-I-T-Y-A <aditya224gupta@gmail.com>
2017-02-26 12:42:55 +0530

    ProfilePic Loader added

--- app/src/main/java/com/decode/dtumessenger/ProfileActivity.java
+++ app/src/main/java/com/decode/dtumessenger/ProfileActivity.java
@@ -1,13 +1,25 @@
 package com.decode.dtumessenger;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView ProfilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +36,45 @@ public class ProfileActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ProfilePic = (ImageView)findViewById(R.id.iv_status);
+
+        new LoadImage().execute("3");
+    }
+
+    public class LoadImage extends AsyncTask<String,String,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String surl = "http://192.168.137.1/"+strings[0]+".jpg";
+            final Bitmap bmp = getBitmapFromURL(surl);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(bmp!=null){
+                        ProfilePic.setImageBitmap(bmp);
+                    }
+                }
+            });
+            return null;
+        }
+    }
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            Log.e("src", src);
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            Log.e("Bitmap","returned");
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
+            return null;
+        }
     }
 }
